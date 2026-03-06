93f642310eeab0c6e031bddb469671ace201c000
Kishore Satheeskumar <kishore.satheeskumar@gmail.com>
2026-03-06 11:52:48 +0000

    Added printToHundred() method

--- Lab6_Q1.java
+++ Lab6_Q1.java
@@ -2,5 +2,12 @@ package Lab6;
 class Lab6_Q1 {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
+        printToHundred();
+    }
+
+    public static void printToHundred() {
+        for (int i=100; i >= 1; i--) {
+            System.out.println(i);
+        }
     }
 }
